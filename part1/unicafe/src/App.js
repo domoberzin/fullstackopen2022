6da52d64fcc61d9245f4994ce640eddf6a24d500
domoberzin <74132255+domoberzin@users.noreply.github.com>
2022-06-10 19:00:42 +0800

    Add files via upload

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.method}>
+    {props.text}
+  </button>
+)
+
+const MostVoteHelper = (props) => {
+  if (props.maxIndex == -1) {
+    return <div>  
+      No quote has more than 0 votes yet
+    </div>
+  } else {
+    return <div>
+      {props.quote}
+      <br></br>
+      has {props.maxVote} votes
+      </div>
+  }
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const selectRand = () => {
+    const nextNum = Math.floor(Math.random() * anecdotes.length)
+    setSelected(nextNum)
+  }
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    console.log(copy)
+    setVotes(copy)
+  }
+
+  const highestVotes = Math.max(...votes) > 0 ? votes.indexOf(Math.max(...votes)) : -1
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br></br>
+       has {votes[selected]} votes
+      <br></br>
+      <Button method = {vote} text = "vote"/>
+      <Button method = {selectRand}text = "next anecdote"/>
+      <h1>Anecdote with most votes</h1>
+      <MostVoteHelper maxIndex = {highestVotes} quote = {anecdotes[highestVotes]} maxVote = {votes[highestVotes]}/>
+    </div>
+  )
+}
+
+export default App
